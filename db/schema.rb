c7cc019c349cf79785e13a59ccf91c037f81b842
Rian Breno Polonini <rianbrenopolonini@gmail.com>
2022-06-18 17:36:30 -0300

    feat: scaffold User

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_18_203132) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_18_203334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
