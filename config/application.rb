1fdaa8d44d1818a8c4e537118ddd80e7eca73061
Rian Breno Polonini <rianbrenopolonini@gmail.com>
2022-06-18 18:36:34 -0300

    feat: add pt-br locales

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Kizame
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+    config.i18n.default_locale = 'pt-BR'
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
