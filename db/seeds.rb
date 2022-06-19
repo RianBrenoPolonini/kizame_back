35066221367fe7aa832e3f903058e73086c5c5f2
Rian Breno Polonini <rianbrenopolonini@gmail.com>
2022-06-19 13:31:37 -0300

    feat: populating db with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,36 @@
-# Modality
+# frozen_string_literal: true
 
+# Modality
 modalities = ['karate', 'jiu-jitsu', 'krav maga', 'zumba', 'muay thai', 'boxe']
 modalities.each { |modality| Modality.create!(name: modality) }
+
+# User
+User.create!(
+  first_name: 'Pedro',
+  last_name: 'Rimes',
+  modality_id: 1,
+  email: 'admin_rico@gmail.com',
+  password: '123456',
+  password_confirmation: '123456',
+  role: 'admin'
+)
+
+User.create!(
+  first_name: 'Lucas',
+  last_name: 'Sei la',
+  modality_id: 2,
+  email: 'professor@gmail.com',
+  password: '123456',
+  password_confirmation: '123456',
+  role: 'teacher'
+)
+
+User.create!(
+  first_name: 'João',
+  last_name: 'Irineu',
+  modality_id: 3,
+  email: 'aluno@gmail.com',
+  password: '123456',
+  password_confirmation: '123456',
+  role: 'student'
+)
